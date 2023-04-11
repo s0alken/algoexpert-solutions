669aa8e3bb652381bb589352c505e1da8ed7f149
s0alken <ropedent@gmail.com>
2023-04-10 20:38:20 -0400

    added sorted-squared-array solution

--- easy/sorted-squared-array/solution.js
+++ easy/sorted-squared-array/solution.js
@@ -1,13 +1,23 @@
-function isValidSubsequence(array, sequence) {
-    let number = sequence.shift();
+function sortedSquaredArray(array) {
+    const sortedSquares = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === number) {
-            number = sequence.shift();
-            if (!number) return true;
+    let smallerValueIdx = 0;
+    let largerValueIdx = array.length - 1;
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        const smallerValue = array[smallerValueIdx];
+        const largerValue = array[largerValueIdx];
+
+        if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+            sortedSquares[i] = smallerValue * smallerValue;
+            smallerValueIdx++;
+        } else {
+            sortedSquares[i] = largerValue * largerValue;
+            largerValueIdx--;
         }
 
     }
 
-    return false;
+    return sortedSquares;
+
 }
